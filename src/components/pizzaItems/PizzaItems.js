469c52b28baa99076df9ec31d4a911014ff1c874
Bekzhan <Human_B_9@mail.ru>
2022-07-14 00:39:24 +0600

    14 07

--- src/components/pizzaItems/PizzaItems.js
+++ src/components/pizzaItems/PizzaItems.js
@@ -1,11 +1,6 @@
 import PizzaItem from "../pizzaItem/PizzaItem";
 
 const PizzaItems = () => {
-  const elements = () => {
-    for (let i = 0; i < 9; i++) {
-      return <PizzaItem />;
-    }
-  };
   return (
     <div className="content__items">
       {[1, 2, 3].map((item) => {
